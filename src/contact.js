3d638cbdbfeef3068a2c65a2ace3eb95acc7f988
Omar Ahmad <superomar121@gmail.com>
2022-02-14 19:22:26 +0400

    added: initial container for contact

--- src/contact.js
+++ src/contact.js
@@ -1,6 +1,15 @@
 import { pageContentDiv } from "./index";
 const loadContactPage = () => {
-    
+    // Create main container
+    const container = document.createElement('div');
+    container.id = 'contact';
+    // Create menu h1
+    const menuTextHeader = document.createElement('h1');
+    menuTextHeader.textContent = 'Contact Us!';
+
+    container.appendChild(menuTextHeader);
+    pageContentDiv.appendChild(container);
+        
 };
 
 export default loadContactPage;
